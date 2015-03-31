521a939e4cf65e037b99dc491dcd3cdcf2710ad7
SpaceKraken <Ethanp678@gmail.com>
2015-03-31 16:58:12 -0500

    Update KrakenBot.java

--- KrakenBot.java
+++ KrakenBot.java
@@ -25,9 +25,8 @@ public class KrakenBot extends PircBot {
             sendMessage(channel, sender + ": Yes?");
         }
         if(message.equalsIgnoreCase("@help")){sendMessage(channel, sender + ": I'm PM'ing you a list of my commands.");
-        sendMessage(sender, "@time-Used to display time. @help-lists all commands.
-        KrakenBot will respond if you call him and is capable of holding a very basic conversation");}
-       
+    sendMessage(sender, "@time-Used to display time. @help-lists all commands. KrakenBot will respond if you call him and is capable of holding a very basic conversation");
+        
         if(message.equalsIgnoreCase("Meet your maker, Krakenbot!")) {
             sendMessage(channel, sender + ": Noo! Don't incur the great Kraken's wrath upon me!");}
         
